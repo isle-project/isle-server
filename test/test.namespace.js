109d02e93d6514b3568154be14f5e4d00998ca7a
Planeshifter <pburckhardt@outlook.com>
2017-12-31 18:58:13 +0100

    Finish namespace tests

--- test/test.namespace.js
+++ test/test.namespace.js
@@ -3,18 +3,20 @@
 // MODULES //
 
 var tape = require( 'tape' );
-
+var isArray = require( '@stdlib/assert/is-array' );
 var waterfall = require( '@stdlib/utils/series-waterfall' );
 var utils = require( './utils.js' );
 var User = require( './../lib/user.js' );
 var Namespace = require( './../lib/namespace.js' );
 
+
+// TESTS //
+
 tape( 'connect to a clean mongoDB database', utils.before );
 
-tape( 'creating a namespace with an owner and a title', function test( t ) {
-
-    function createUser( next ) {
-        var u = [
+tape( 'successfully creates a namespace with an owner and a title', function test( t ) {
+	function createUser( next ) {
+		var u = [
 			{
 				'email': 'lotti.anton.super@gmail.com',
 				'password': 'hans'
@@ -27,21 +29,21 @@ tape( 'creating a namespace with an owner and a title', function test( t ) {
 				next( null, users );
 			}
 		});
-    }
+	}
 
-
-    function createNamespace( members, next ) {
+	function createNamespace( owners, next ) {
 		var o = {
 			'title': 'First_Namespace',
-            'members': members,
-            'description': 'The first namespace'
+			'owners': owners,
+			'description': 'The first namespace'
 		};
 		Namespace.create( o, function onCreate( err, createdNamespace ) {
 			if ( err ) {
 				return next( err );
 			}
 			t.strictEqual( createdNamespace.title, 'First_Namespace', 'has correct title' );
-			t.ok( isArray( createdNamespace.members ), 'has members' );
+			t.ok( isArray( createdNamespace.owners ), 'has owners' );
+			t.strictEqual( createdNamespace.description, 'The first namespace', 'has correct description' );
 			next();
 		});
 	}
@@ -55,5 +57,100 @@ tape( 'creating a namespace with an owner and a title', function test( t ) {
 		t.end();
 	}
 	waterfall( [ createUser, createNamespace ], done );
-
 });
+
+tape( 'fails creating a namespace with an already used title', function test( t ) {
+	function createUser( next ) {
+		var u = [
+			{
+				'email': 'peter.flux.super@gmail.com',
+				'password': 'fluxus'
+			}
+		];
+		User.create( u, function onCreate( err, users ) {
+			if ( err ) {
+				next( err );
+			} else {
+				next( null, users );
+			}
+		});
+	}
+
+	function createNamespace( owners, next ) {
+		var o = {
+			'title': 'First_Namespace',
+			'owners': owners,
+			'description': 'Namespace with already taken title'
+		};
+		Namespace.create( o, function onCreate( err ) {
+			t.strictEqual( err instanceof Error, true, 'returns an error' );
+			next( err );
+		});
+	}
+
+	function done( error ) {
+		if ( error ) {
+			t.pass( 'should return an error' );
+		} else {
+			t.fail( 'executed without errors' );
+		}
+		t.end();
+	}
+	waterfall( [ createUser, createNamespace ], done );
+});
+
+tape( 'fails creating a namespace without a title', function test( t ) {
+	function createUser( next ) {
+		var u = [
+			{
+				'email': 'hans.anton.super@gmail.com',
+				'password': 'lotti'
+			}
+		];
+		User.create( u, function onCreate( err, users ) {
+			if ( err ) {
+				next( err );
+			} else {
+				next( null, users );
+			}
+		});
+	}
+
+	function createNamespace( owners, next ) {
+		var o = {
+			'owners': owners,
+			'description': 'Namespace without title'
+		};
+		Namespace.create( o, function onCreate( err ) {
+			var expected = 'Namespace validation failed: title: Path `title` is required.';
+			t.strictEqual( err instanceof Error, true, 'returns an error' );
+			t.strictEqual( err.message, expected, 'has expected message' );
+			next( err );
+		});
+	}
+
+	function done( error ) {
+		if ( error ) {
+			t.pass( 'should return an error' );
+		} else {
+			t.fail( 'executed without errors' );
+		}
+		t.end();
+	}
+	waterfall( [ createUser, createNamespace ], done );
+});
+
+tape( 'fails creating a namespace without owners', function test( t ) {
+	var o = {
+		'title': 'ownerless namespace',
+		'description': 'Namespace without an owner'
+	};
+	Namespace.create( o, function onCreate( err ) {
+		var expected = 'Namespace validation failed: owners: Namespaces need at least one owner';
+		t.strictEqual( err instanceof Error, true, 'returns an error' );
+		t.strictEqual( err.message, expected, 'has expected message' );
+		t.end();
+	});
+});
+
+tape( 'perform clean-up', utils.after );
