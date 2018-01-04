27b58da64d6605d6799b82b734f0810c6d8ca489
Planeshifter <pburckhardt@outlook.com>
2018-01-04 20:46:53 +0100

    Add tests

--- test/test.js
+++ test/test.js
@@ -5,8 +5,14 @@
 var tape = require( 'tape' );
 var request = require( 'supertest' );
 var proxyquire = require( 'proxyquire' );
+var isValidObjectId = require( 'mongoose' ).Types.ObjectId.isValid;
+var isObject = require( '@stdlib/assert/is-object' );
+var isString = require( '@stdlib/assert/is-string' );
+var isArray = require( '@stdlib/assert/is-array' );
+var isNull = require( '@stdlib/assert/is-null' );
 var noop = require( '@stdlib/utils/noop' );
 var copy = require( '@stdlib/utils/copy' );
+var User = require( './../lib/user.js' );
 var utils = require( './utils.js' );
 
 var requires = {
@@ -30,9 +36,17 @@ var requires = {
 var app = proxyquire( './../lib/index.js', requires );
 
 
+// VARIABLES //
+
+var USER_TOKEN;
+var USER_ID;
+
+
 // TESTS //
 
 tape( 'connect to a clean mongoDB database', utils.before );
+
+tape( 'populate the database', utils.populateDatabase );
 
 tape( 'GET /', function test( t ) {
 	request( app )
@@ -135,44 +149,443 @@ tape( 'GET /forgot_password - failure sending email', function test( t ) {
 	});
 });
 
-/*
+tape( 'GET /has_write_access (no write access)', function test( t ) {
+	request( app )
+	.get( '/has_write_access' )
+	.query({ email: 'fridolin.supertester@gmail.com' })
+	.expect( 200 )
+	.end( function onEnd( err, res ) {
+		t.error( err, 'does not return an error');
+		var obj = JSON.parse( res.text );
+		t.strictEqual( obj.message, 'The user has no write access', 'returns expected message' );
+		t.strictEqual( obj.writeAccess, false, 'returns expected write access' );
+		t.end();
+	});
+});
+
+tape( 'GET /has_write_access (write access)', function test( t ) {
+	var email = 'fridolin.supertester@gmail.com';
+	User.findOneAndUpdate({ email: email },
+		{
+			$set: { writeAccess: true }
+		},
+		function onDone( err ) {
+			request( app )
+			.get( '/has_write_access' )
+			.query({ email: email })
+			.expect( 200 )
+			.end( function onEnd( err, res ) {
+				t.error( err, 'does not return an error');
+				var obj = JSON.parse( res.text );
+				t.strictEqual( obj.message, 'The user has write access', 'returns expected message' );
+				t.strictEqual( obj.writeAccess, true, 'returns expected write access' );
+				t.end();
+			});
+		}
+	);
+});
+
 tape( 'GET /get_lesson_info', function test( t ) {
-
+	request( app )
+	.get( '/get_lesson_info' )
+	.query({ lessonName: 'Why have you followed me?', namespaceName: 'Frankenstein meets the Wolf Man' })
+	.expect( 200 )
+	.end( function onEnd( err, res ) {
+		t.error( err, 'does not return an error' );
+		var obj = JSON.parse( res.text );
+		t.ok( isString( obj.lessonID ), 'returns ID of lesson' );
+		t.ok( isString( obj.namespaceID ), 'returns ID of namespace' );
+		t.end();
+	});
 });
 
-tape( 'GET /has_write_access', function test( t ) {
-
-});
-
-tape( 'POST /send_mail', function test( t ) {
-
+tape( 'GET /get_lesson_info (unknown lesson)', function test( t ) {
+	request( app )
+	.get( '/get_lesson_info' )
+	.query({ lessonName: 'Unknown monster', namespaceName: 'Frankenstein meets the Wolf Man' })
+	.expect( 410 )
+	.end( function onEnd( err, res ) {
+		t.error( err, 'does not return an error' );
+		t.strictEqual( res.text, 'Lesson was not found.', 'returns expected message' );
+		t.end();
+	});
 });
 
 tape( 'GET /get_lesson', function test( t ) {
+	request( app )
+	.get( '/get_lesson' )
+	.query({ lessonName: 'Why have you followed me?', namespaceName: 'Frankenstein meets the Wolf Man' })
+	.expect( 200 )
+	.end( function onEnd( err, res ) {
+		t.error( err, 'does not return an error' );
+		var obj = JSON.parse( res.text );
+		t.strictEqual( obj.message, 'ok', 'returns message' );
+		t.strictEqual( isObject( obj.lesson ), true, 'returns lesson object' );
+		t.strictEqual( obj.lesson.title, 'Why have you followed me?', 'returns correct title' );
+		t.end();
+	});
+});
 
+tape( 'GET /get_lesson (unknown lesson)', function test( t ) {
+	request( app )
+	.get( '/get_lesson' )
+	.query({ lessonName: 'Why have you forsaken me?', namespaceName: 'Frankenstein meets the Wolf Man' })
+	.expect( 404 )
+	.end( function onEnd( err, res ) {
+		t.error( err, 'does not return an error' );
+		t.strictEqual( res.text, 'Lesson query failed.', 'returns message' );
+		t.end();
+	});
+});
+
+tape( 'GET /get_lesson (unknown namespace)', function test( t ) {
+	request( app )
+	.get( '/get_lesson' )
+	.query({ lessonName: 'Why have you followed me?', namespaceName: 'Frankenstein kills the Wolf Man' })
+	.expect( 404 )
+	.end( function onEnd( err, res ) {
+		t.error( err, 'does not return an error' );
+		t.strictEqual( res.text, 'Namespace query failed.', 'returns message' );
+		t.end();
+	});
+});
+
+tape( 'GET /get_lesson (invalid `lessonName`)', function test( t ) {
+	request( app )
+	.get( '/get_lesson' )
+	.query({ lessonName: [], namespaceName: 'Frankenstein meets the Wolf Man' })
+	.expect( 400 )
+	.end( function onEnd( err, res ) {
+		t.error( err, 'does not return an error' );
+		t.strictEqual( res.text, '`lessonName` has to be a string', 'returns message' );
+		t.end();
+	});
+});
+
+tape( 'GET /get_lesson (invalid `namespaceName`)', function test( t ) {
+	request( app )
+	.get( '/get_lesson' )
+	.query({ lessonName: 'Why have you followed me?', namespaceName: []})
+	.expect( 400 )
+	.end( function onEnd( err, res ) {
+		t.error( err, 'does not return an error' );
+		t.strictEqual( res.text, '`namespaceName` has to be a string', 'returns message' );
+		t.end();
+	});
 });
 
 tape( 'GET /get_public_lessons', function test( t ) {
+	request( app )
+	.get( '/get_public_lessons' )
+	.expect( 200 )
+	.end( function onEnd( err, res ) {
+		t.error( err, 'does not return an error' );
+		var body = res.body;
+		t.strictEqual( body.message, 'ok', 'returns expected message' );
+		t.ok( isArray( body.lessons ), 'returns array of public lessons' );
+		t.strictEqual( body.lessons.length, 4, 'array has expected length' );
+		t.end();
+	});
+});
 
+tape( 'GET /get_lessons (invalid `namespaceName`)', function test( t ) {
+	request( app )
+	.get( '/get_lessons' )
+	.query({ namespaceName: []})
+	.expect( 400 )
+	.end( function onEnd( err, res ) {
+		t.error( err, 'does not return an error' );
+		t.strictEqual( res.text, '`namespaceName` has to be a string.', 'returns message' );
+		t.end();
+	});
 });
 
 tape( 'GET /get_lessons', function test( t ) {
+	request( app )
+	.get( '/get_lessons' )
+	.query({ namespaceName: 'Frankenstein meets the Wolf Man' })
+	.expect( 200 )
+	.end( function onEnd( err, res ) {
+		t.error( err, 'does not return an error' );
+		var body = res.body;
+		t.strictEqual( body.message, 'ok', 'returns expected message' );
+		t.ok( isArray( body.lessons ), 'returns expected array' );
+		t.strictEqual( body.lessons.length, 3, 'array has expected length' );
+		t.end();
+	});
+});
 
+tape( 'POST /update_user_password (invalid ID)', function test( t ) {
+	request( app )
+	.post( '/update_user_password' )
+	.send({ id: 'abc', newPassword: 'zorro123' })
+	.expect( 400 )
+	.end( function onEnd( err, res ) {
+		t.error( err, 'does not return an error' );
+		t.strictEqual( res.text, '`id` has to be a valid ObjectID', 'returns expected message' );
+		t.end();
+	});
+});
+
+tape( 'POST /update_user_password (invalid `newPassword`)', function test( t ) {
+	User.findOne( function onUser( err, user ) {
+		request( app )
+		.post( '/update_user_password' )
+		.send({ id: user._id, newPassword: [] })
+		.expect( 400 )
+		.end( function onEnd( err, res ) {
+			t.error( err, 'does not return an error' );
+			t.strictEqual( res.text, 'New password has to be a string', 'returns expected message' );
+			t.end();
+		});
+	});
 });
 
 tape( 'POST /update_user_password', function test( t ) {
+	User.findOne( function onUser( err, user ) {
+		request( app )
+		.post( '/update_user_password' )
+		.send({ id: user._id, newPassword: 'zorro123' })
+		.expect( 200 )
+		.end( function onEnd( err, res ) {
+			t.error( err, 'does not return an error' );
+			var body = res.body;
+			t.strictEqual( body.message, 'User password successfully updated.', 'returns expected message' );
+			t.end();
+		});
+	});
+});
 
+tape( 'POST /update_user_password (unknown user)', function test( t ) {
+	request( app )
+	.post( '/update_user_password' )
+	.send({ id: '5a4e409c3d8668487003dec6', newPassword: 'zorro123' })
+	.expect( 404 )
+	.end( function onEnd( err, res ) {
+		t.error( err, 'does not return an error' );
+		t.strictEqual( res.text, 'User does not exist.', 'returns expected message' );
+		t.end();
+	});
+});
+
+tape( 'POST /send_mail', function test( t ) {
+	request( app )
+	.post( '/send_mail' )
+	.send({
+		from: 'harry.potter123@gmx.de',
+		to: 'hermine.granger777@gmx.de',
+		text: 'I am hungry'
+	})
+	.expect( 200 )
+	.end( function onEnd( err, res ) {
+		t.error( err, 'does not return an error' );
+		t.strictEqual( res.body, 'Mail sent', 'returns expected message' );
+		t.end();
+	});
+});
+
+tape( 'POST /login (invalid email)', function test( t ) {
+	request( app )
+	.post( '/login' )
+	.send({
+		email: [],
+		password: 'Lon Chaney Jr.'
+	})
+	.expect( 400 )
+	.end( function onEnd( err, res ) {
+		t.error( err, 'does not return an error' );
+		t.strictEqual( res.text, '`email` has to be a string', 'returns expected message' );
+		t.end();
+	});
+});
+
+tape( 'POST /login (invalid password)', function test( t ) {
+	request( app )
+	.post( '/login' )
+	.send({
+		email: 'wolfman666@gmail.com',
+		password: []
+	})
+	.expect( 400 )
+	.end( function onEnd( err, res ) {
+		t.error( err, 'does not return an error' );
+		t.strictEqual( res.text, '`password` has to be a string', 'returns expected message' );
+		t.end();
+	});
+});
+
+tape( 'POST /login (unknown email address)', function test( t ) {
+	request( app )
+	.post( '/login' )
+	.send({
+		email: 'wolfman667@gmail.com',
+		password: 'Lon Chaney Jr.'
+	})
+	.expect( 404 )
+	.end( function onEnd( err, res ) {
+		t.error( err, 'does not return an error' );
+		var body = res.body;
+		t.strictEqual( body.message, 'No user with the given email address found.', 'returns expected message' );
+		t.strictEqual( body.type, 'no_user', 'returns expected type' );
+		t.end();
+	});
+});
+
+tape( 'POST /login (wrong password)', function test( t ) {
+	request( app )
+	.post( '/login' )
+	.send({
+		email: 'wolfman666@gmail.com',
+		password: 'Lon Chaney Sr.'
+	})
+	.expect( 401 )
+	.end( function onEnd( err, res ) {
+		t.error( err, 'does not return an error' );
+		var body = res.body;
+		t.strictEqual( body.message, 'Password is not correct.', 'returns expected message' );
+		t.strictEqual( body.type, 'incorrect_password', 'returns expected type' );
+		t.end();
+	});
 });
 
 tape( 'POST /login', function test( t ) {
-
+	request( app )
+	.post( '/login' )
+	.send({
+		email: 'dracula@gmail.com',
+		password: 'Bela Lugosi'
+	})
+	.expect( 200 )
+	.end( function onEnd( err, res ) {
+		t.error( err, 'does not return an error' );
+		var body = res.body;
+		t.strictEqual( body.message, 'ok', 'returns expected message' );
+		t.ok( isString( body.token ), 'returns `token` string' );
+		USER_TOKEN = body.token;
+		t.ok( isValidObjectId( body.id ), 'returns valid `id`' );
+		USER_ID = body.id;
+		t.end();
+	});
 });
+
+tape( 'POST /credentials', function test( t ) {
+	request( app )
+	.post( '/credentials' )
+	.set( 'Authorization', 'JWT '+USER_TOKEN )
+	.send({
+		id: USER_ID
+	})
+	.expect( 200 )
+	.end( function onEnd( err, res ) {
+		t.error( err, 'does not return an error' );
+		t.ok( isObject( res.body ), 'returns a user object' );
+		t.strictEqual( res.body.id, USER_ID, 'has expected ID' );
+		t.strictEqual( res.body.email, 'dracula@gmail.com', 'has expected email' );
+		t.strictEqual( res.body.name, 'Count Dracula', 'has expected name' );
+		t.strictEqual( res.body.writeAccess, false, 'has expected writeAccess' );
+		t.end();
+	});
+});
+
+/*
 
 tape( 'POST /store_session_element', function test( t ) {
 
 });
 
 tape( 'POST /retrieve_data', function test( t ) {
+
+});
+
+tape( 'POST /get_user_rights', function test( t ) {
+
+});
+
+tape( 'GET /set_write_access', function test( t ) {
+
+});
+
+tape( 'GET /copy_lesson', function test( t ) {
+
+});
+
+tape( 'POST /create_lesson', function test( t ) {
+
+});
+
+tape( 'GET /delete_lesson', function test( t ) {
+
+});
+
+tape( 'GET /update_lesson', function test( t ) {
+
+});
+
+tape( 'GET /activate_lesson', function test( t ) {
+
+});
+
+tape( 'GET /deactivate_lesson', function test( t ) {
+
+});
+
+tape( 'GET /show_lesson', function test( t ) {
+
+});
+
+tape( 'GET /hide_lesson', function test( t ) {
+
+});
+
+tape( 'POST /create_namespace', function test( t ) {
+
+});
+
+tape( 'GET /get_namespaces', function test( t ) {
+
+});
+
+tape( 'GET /delete_namespace', function test( t ) {
+
+});
+
+tape( 'POST /update_namespace', function test( t ) {
+
+});
+
+tape( 'POST /update_user', function test( t ) {
+
+});
+
+tape( 'GET /delete_session_element', function test( t ) {
+
+});
+
+tape( 'POST /get_user_actions', function test( t ) {
+
+});
+
+tape( 'POST /get_current_user_actions', function test( t ) {
+
+});
+
+tape( 'POST /create_cohort', function test( t ) {
+
+});
+
+tape( 'GET /get_cohorts', function test( t ) {
+
+});
+
+tape( 'GET /delete_cohort', function test( t ) {
+
+});
+
+tape( 'POST /update_cohort', function test( t ) {
+
+});
+
+tape( 'POST /upload_file', function test( t ) {
 
 });
 */
--- test/utils.js
+++ test/utils.js
@@ -5,7 +5,10 @@
 var mongoose = require( 'mongoose' );
 var setReadOnly = require( '@stdlib/utils/define-read-only-property' );
 var hasOwnProp = require( '@stdlib/assert/has-own-property' );
+var waterfall = require( '@stdlib/utils/series-waterfall' );
 var noop = require( '@stdlib/utils/noop' );
+var Namespace = require( './../lib/namespace.js' );
+var Lesson = require( './../lib/lesson.js' );
 var User = require( './../lib/user.js' );
 
 
@@ -44,6 +47,113 @@ setReadOnly( ns, 'before', function before( t ) {
 	} else {
 		return clearDB();
 	}
+});
+
+setReadOnly( ns, 'populateDatabase', function populateDatabase( t ) {
+	function createUsers( next ) {
+		var pop = [
+			{
+				'email': 'zorro707@gmail.com',
+				'name': 'Zorro',
+				'password': 'zorro_is_the_best'
+			},
+			{
+				'email': 'wolfman666@gmail.com',
+				'name': 'The Wolfman',
+				'password': 'Lon Chaney Jr.'
+			},
+			{
+				'email': 'dracula@gmail.com',
+				'name': 'Count Dracula',
+				'password': 'Bela Lugosi'
+			},
+			{
+				'email': 'ed_wood@gmail.com',
+				'name': 'Ed Wood',
+				'password': 'Plan 9 from Outer Space'
+			},
+			{
+				'email': 'mummy@gmail.com',
+				'name': 'The Mummy',
+				'password': 'Egypt'
+			},
+			{
+				'email': 'frankenstein@gmail.com',
+				'name': 'Boris Karloff',
+				'password': 'Mary Shelly'
+			}
+		];
+		User.create( pop, next );
+	}
+
+	function createNamespaces( users, next ) {
+		var namespaces = [
+			{
+				'title': 'Frankenstein meets the Wolf Man',
+				'description': 'Open the grave of Larry Talbot',
+				'owners': [ users[ 1 ]._id, users[ 5 ]._id ]
+			},
+			{
+				'title': 'Dracula vs. the Wolf Man',
+				'description': 'A great match',
+				'owners': [ users[ 1 ]._id, users[ 2 ]._id ]
+			},
+			{
+				'title': 'Dracula vs. Frankenstein',
+				'description': 'Dracula unearthes Frankenstein',
+				'owners': [ users[ 2 ]._id, users[ 5 ]._id ]
+			}
+		];
+		Namespace.create( namespaces, next );
+	}
+
+	function createLessons( namespaces, next ) {
+		var lessons = [
+			{
+				namespace: namespaces[ 2 ]._id,
+				title: 'Unearth the monster!',
+				public: true
+			},
+			{
+				namespace: namespaces[ 2 ]._id,
+				title: 'Kill the cemetery keeper!',
+				public: true
+			},
+			{
+				namespace: namespaces[ 2 ]._id,
+				title: 'Drink his blood!',
+				public: false
+			},
+			{
+				namespace: namespaces[ 0 ]._id,
+				title: 'Why have you followed me?',
+				public: true
+			},
+			{
+				namespace: namespaces[ 0 ]._id,
+				title: 'Talbot, you are a murderer!',
+				public: true
+			},
+			{
+				namespace: namespaces[ 0 ]._id,
+				title: 'Prove it!',
+				public: false
+			}
+		];
+		Lesson.create( lessons, next );
+	}
+
+	function done( err, res ) {
+		if ( err ) {
+			t.fail( 'should not return an error' );
+		} else {
+			t.pass( 'executed without errors' );
+		}
+		setTimeout( function onTimeout() {
+			t.end();
+		}, 1000 );
+	}
+	waterfall([ createUsers, createNamespaces, createLessons ], done );
 });
 
 setReadOnly( ns, 'after', function after( t ) {
