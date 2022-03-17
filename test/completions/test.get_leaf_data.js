1122fe0101e2e783335e7b9ca86c2603d6a2e1e3
Philipp Burckhardt <pburckhardt@outlook.com>
2022-03-17 13:36:10 -0400

    fix: Handle that getLeafData is async function

--- test/completions/test.get_leaf_data.js
+++ test/completions/test.get_leaf_data.js
@@ -59,9 +59,15 @@ tape( 'should return an array of objects', ( t ) => {
 					writable: true,
 					enumerable: false
 				});
-				const arr = getLeafData( 'completed', nodes, null, users );
-				t.ok( isObjectArray( arr ), 'returns an array of objects' );
-				t.end();
+				getLeafData( 'completed', nodes, null, users )
+					.then( ( arr ) => {
+						t.ok( isObjectArray( arr ), 'returns an array of objects' );
+						t.end();
+					})
+					.catch( err => {
+						t.error( err );
+						t.end();
+					});
 			});
 	});
 });
@@ -75,13 +81,19 @@ tape( 'should return an object array with each object having a userId key', ( t 
 					writable: true,
 					enumerable: false
 				});
-				const arr = getLeafData( 'completed', nodes, null, users );
-				const userIds = new Set( users.map( v => v._id ) );
-				t.ok( arr.every( a => {
-					const keys = Object.keys( a );
-					return keys.every( k => userIds.has( k ) );
-				}), 'has user ID keys' );
-				t.end();
+				getLeafData( 'completed', nodes, null, users )
+					.then( ( arr ) => {
+						const userIds = new Set( users.map( v => v._id ) );
+						t.ok( arr.every( a => {
+							const keys = Object.keys( a );
+							return keys.every( k => userIds.has( k ) );
+						}), 'has user ID keys' );
+						t.end();
+					})
+					.catch( err => {
+						t.error( err );
+						t.end();
+					});
 			});
 	});
 });
@@ -95,16 +107,22 @@ tape( 'should return an object array with each object having values with keys `v
 					writable: true,
 					enumerable: false
 				});
-				const arr = getLeafData( 'completed', nodes, null, users );
-				t.ok( arr.every( a => {
-					const values = objectValues( a );
-					return values.every( v =>
-						hasOwnProp( v, 'value' ) &&
-						hasOwnProp( v, 'time' ) &&
-						hasOwnProp( v, 'tag' )
-					);
-				}), 'has keys `value`, `time`, and `tag`' );
-				t.end();
+				getLeafData( 'completed', nodes, null, users )
+					.then( ( arr ) => {
+						t.ok( arr.every( a => {
+							const values = objectValues( a );
+							return values.every( v =>
+								hasOwnProp( v, 'value' ) &&
+								hasOwnProp( v, 'time' ) &&
+								hasOwnProp( v, 'tag' )
+							);
+						}), 'has keys `value`, `time`, and `tag`' );
+						t.end();
+					})
+					.catch( err => {
+						t.error( err );
+						t.end();
+					});
 			});
 	});
 });
