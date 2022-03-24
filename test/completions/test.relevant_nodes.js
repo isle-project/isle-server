104235b5c2c15d060bd25454fe63b4b766921fb9
Philipp Burckhardt <pburckhardt@outlook.com>
2022-03-24 16:21:51 -0400

    fix: Update relevantNodes and add tests

---
+++ test/completions/test.relevant_nodes.js
@@ -0,0 +1,56 @@
+/**
+* Copyright (C) 2016-present The ISLE Authors
+*
+* The isle-server program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+/* eslint-disable max-nested-callbacks */
+
+'use strict';
+
+// MODULES //
+
+const tape = require( 'tape' );
+const isStringArray = require( '@stdlib/assert/is-string-array' );
+const { relevantNodes } = require( './../../lib/helpers/completions.js' );
+const Lesson = require( './../../lib/models/lesson.js' );
+const utils = require( './../utils.js' );
+
+
+// TESTS //
+
+tape( 'main export is a function', ( t ) => {
+	t.ok( typeof relevantNodes === 'function', 'main export is a function' );
+	t.end();
+});
+
+tape( 'connect to a clean mongoDB database', utils.before );
+
+tape( 'populate the database', utils.populateDatabase );
+
+tape( 'the function should return an array of node IDs that match the completion criteria and the level (lesson level)', ( t ) => {
+	Lesson.findOne({
+		title: 'Unearth the monster'
+	})
+		.then( ( lesson ) => {
+			relevantNodes( lesson._id, 'lesson', [ 'all' ], null )
+				.then( ( arr ) => {
+					t.ok( isStringArray( arr ), 'returns an array of strings' );
+					t.strictEqual( arr.length, 2, 'returns an array of length 2' );
+					t.ok( arr.includes( 'free-text-question-1' ), 'contains the expected node ID' );
+					t.ok( arr.includes( 'free-text-question-2' ), 'contains the expected node ID' );
+					t.end();
+				});
+		});
+});
